d80745ec8ba83b498cff95dc45dabe4f59287bd9
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2020-10-29 19:35:48 +0000

    remove unnecessary script

--- performance_tests/setup_performance_test.sh
+++ performance_tests/setup_performance_test.sh
@@ -78,4 +78,37 @@ done
 # decompress the genome and input files for Sigma
 gzip -k -d sigma_db_5636_species/*/*.fasta.gz
 
+# download a large HMP sample
+mkdir -p inputs
+wget --quiet http://downloads.hmpdacc.org/dacc/hhs/genome/microbiome/wgs/analysis/hmwgsqc/v2/SRS078671.tar.bz2 \
+  -O inputs/SRS078671.tar.bz2
+
+# decompress the downloaded file
+tar -xvjf inputs/SRS078671.tar.bz2 -C inputs/
+
+# concatenate the files into one big file
+cat inputs/SRS078671/SRS078671.denovo_duplicates_marked.trimmed.1.fastq \
+  inputs/SRS078671/SRS078671.denovo_duplicates_marked.trimmed.singleton.fastq > \
+  inputs/SRS078671_aggregate.fastq
+
+# subsample the file into smaller samples
+read_counts=(input_10K input_100K input_1M input_10M input_100M)
+size=1000
+
+for read_count in "${read_counts[@]}"; do
+  echo "Subsampling for ${read_count}"
+  size=$((size*10))
+  seqtk sample inputs/SRS078671_aggregate.fastq $size > inputs/"${read_count}"_reads.fastq
+done
+
+# compress the files
+for read_count in "${read_counts[@]}"; do
+  echo "Compressing the subsampled file for ${read_count}"
+  gzip -c inputs/"${read_count}"_reads.fastq > inputs/"${read_count}"_reads.fastq.gz
+done
+
+# removing files that are not going to be used
+rm inputs/SRS078671_aggregate.fastq
+rm -r inputs/SRS078671
+
 echo "DONE!"

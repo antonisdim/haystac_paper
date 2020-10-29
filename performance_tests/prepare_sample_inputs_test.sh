c2f036a0a4cb211b4fff64be91d7c752b3051836
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2020-10-29 19:03:39 +0000

    removing unnecessary files

--- performance_tests/prepare_sample_inputs_test.sh
+++ performance_tests/prepare_sample_inputs_test.sh
@@ -19,10 +19,9 @@ cat inputs/SRS078671/SRS078671.denovo_duplicates_marked.trimmed.1.fastq \
   inputs/SRS078671_aggregate.fastq
 
 # subsample the file into smaller samples
-
 read_counts=(input_10K input_100K input_1M input_10M input_100M)
-
 size=1000
+
 for read_count in "${read_counts[@]}"; do
   echo "Subsampling for ${read_count}"
   size=$((size*10))
@@ -30,8 +29,6 @@ for read_count in "${read_counts[@]}"; do
 done
 
 # compress the files
-
-
 for read_count in "${read_counts[@]}"; do
   echo "Compressing the subsampled file for ${read_count}"
   gzip -c inputs/"${read_count}"_reads.fastq > inputs/"${read_count}"_reads.fastq.gz

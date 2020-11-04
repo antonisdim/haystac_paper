677b02b1c163a0df1982e8eec29ef7a87d5e341d
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2020-11-04 16:45:06 +0000

    correct test

--- performance_tests/run_performance_tests.sh
+++ performance_tests/run_performance_tests.sh
@@ -20,9 +20,6 @@ MAX_MEM=$(free -m | awk '/^Mem:/{printf "%.0f", $2}')
 # list of database sizes (i.e. number of species) to test
 species_counts=(10 100)
 
-# list of library sizes to test
-read_counts=(input_10K input_100K)
-
 # make a folder to store the log files
 mkdir -p logs
 
@@ -36,24 +33,18 @@ for species_count in "${species_counts[@]}"; do
   for mem in $MAX_MEM; do
     echo "Running 'run_haystack_db_mem.sh' for ${species_count} species and ${mem} RAM"
 
-    # delete any existing indices outputs so we can rebuild them
-    rm -f ./haystack_genome_cache_small/ncbi/*/*.bt2l
-    rm -f ./haystack_genome_cache_small/ncbi/*/*.fasta.gz.fai
-
     $time -v -o "logs/haystack_db_mem-${species_count}_species-${mem}_mem.time.log" \
       bash scripts/run_haystack_db_mem.sh "${species_count}_species" "${mem}" \
       >"logs/haystack_db_mem-${species_count}_species-${mem}_mem.log"
   done
 done
 
-# analysing samples of different sizes against a db of 5636 species with conda as a package manager
-for read_count in "${read_counts[@]}"; do
 
-  for bool in "False" "True"; do
-    echo "Running 'run_haystack_samples.sh' for ${read_count} reads, against 5636 species and use_conda ${bool}"
+# analysing a sample of 1M reads against dbs of various sizes
+for species_count in "${species_counts[@]:0:4}"; do
+  echo "Running 'run_haystack_samples.sh' for input_10K reads, against ${species_count} species and use_conda True"
 
-    $time -v -o "logs/haystack_samples-${read_count}_reads-5636_species-conda_${bool}.time.log" \
-      bash scripts/run_haystack_samples.sh "${read_count}_reads" 5636_species "${bool}" \
-      >"logs/haystack_samples-${read_count}_reads-5636_species-conda_${bool}.log"
-  done
+  $time -v -o "logs/haystack_samples-input_10K_reads-${species_count}_species-conda_True.time.log" \
+    bash scripts/run_haystack_samples.sh input_10K_reads "${species_count}_species" "True" \
+    >"logs/haystack_samples-input_10K_reads-${species_count}_species-conda_True.log"
 done

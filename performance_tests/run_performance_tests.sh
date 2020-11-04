cbfda16d62befec7c5be18ade75053a6e7b61d8e
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2020-11-04 16:09:29 +0000

    test just for haystack

--- performance_tests/run_performance_tests.sh
+++ performance_tests/run_performance_tests.sh
@@ -7,7 +7,7 @@
 
 # activate the conda environment for running the tests
 eval "$(conda shell.bash hook)"
-conda activate performance_test
+conda activate small_test
 
 # bash strict mode (set after conda is activated, as the start-up scripts are not strict safe)
 set -euo pipefail
@@ -18,10 +18,10 @@ time=$(which time)
 MAX_MEM=$(free -m | awk '/^Mem:/{printf "%.0f", $2}')
 
 # list of database sizes (i.e. number of species) to test
-species_counts=(10 100 500 1000 5636)
+species_counts=(10 100)
 
 # list of library sizes to test
-read_counts=(input_10K input_100K input_1M input_10M input_100M)
+read_counts=(input_10K input_100K)
 
 # make a folder to store the log files
 mkdir -p logs
@@ -37,8 +37,8 @@ for species_count in "${species_counts[@]}"; do
     echo "Running 'run_haystack_db_mem.sh' for ${species_count} species and ${mem} RAM"
 
     # delete any existing indices outputs so we can rebuild them
-    rm -f ./haystack_genome_cache/ncbi/*/*.bt2l
-    rm -f ./haystack_genome_cache/ncbi/*/*.fasta.gz.fai
+    rm -f ./haystack_genome_cache_small/ncbi/*/*.bt2l
+    rm -f ./haystack_genome_cache_small/ncbi/*/*.fasta.gz.fai
 
     $time -v -o "logs/haystack_db_mem-${species_count}_species-${mem}_mem.time.log" \
       bash scripts/run_haystack_db_mem.sh "${species_count}_species" "${mem}" \
@@ -56,103 +56,4 @@ for read_count in "${read_counts[@]}"; do
       bash scripts/run_haystack_samples.sh "${read_count}_reads" 5636_species "${bool}" \
       >"logs/haystack_samples-${read_count}_reads-5636_species-conda_${bool}.log"
   done
-done
-
-# analysing a sample of 1M reads against dbs of various sizes
-for species_count in "${species_counts[@]:0:4}"; do
-  echo "Running 'run_haystack_samples.sh' for input_1M reads, against ${species_count} species and use_conda True"
-
-  $time -v -o "logs/haystack_samples-input_1M_reads-${species_count}_species-conda_True.time.log" \
-    bash scripts/run_haystack_samples.sh input_1M_reads "${species_count}_species" "True" \
-    >"logs/haystack_samples-input_1M_reads-${species_count}_species-conda_True.log"
-done
-
-# ----------------------------------------------------------------------------------------------------------------------
-# -                                   run all the tests for kraken/bracken                                             -
-# ----------------------------------------------------------------------------------------------------------------------
-
-# building databases of different sizes
-for species_count in "${species_counts[@]}"; do
-  echo "Running 'run_kraken_db.sh' for ${species_count} species"
-
-  $time -v -o "logs/kraken_db-${species_count}_species.time.log" \
-    bash scripts/run_kraken_db.sh "${species_count}_species" >"kraken_db-${species_count}_species.log"
-done
-
-# analysing samples of different sizes against a db of 5636 species
-for read_count in "${read_counts[@]}"; do
-  echo "Running 'run_kraken_samples.sh' for ${read_count} reads against 5636 species"
-
-  $time -v -o "logs/kraken_samples-${read_count}_reads-5636_species.time.log" \
-    bash scripts/run_kraken_samples.sh "${read_count}" 5636 \
-    >"logs/kraken_samples-${read_count}_reads-5636_species.log"
-done
-
-# analysing a sample of 1M reads against dbs of various sizes
-for species_count in "${species_counts[@]:0:4}"; do
-  echo "Running 'run_kraken_samples.sh' for input_1M reads against ${species_count} species"
-
-  $time -v -o "logs/kraken_samples-input_1M-${species_count}_species.time.log" \
-    bash scripts/run_kraken_samples.sh input_1M "${species_count}" \
-    >"logs/kraken_samples-input_1M-${species_count}_species.log"
-done
-
-# ----------------------------------------------------------------------------------------------------------------------
-# -                                       run all the tests for sigma                                                  -
-# ----------------------------------------------------------------------------------------------------------------------
-
-# building databases of different sizes
-for species_count in "${species_counts[@]}"; do
-  echo "Running 'run_sigma_db.sh' for ${species_count} species"
-
-  # delete any existing indices so we can rebuild them
-  rm -f ./sigma_db_"${species_count}_species"/*/*.bt2
-
-  $time -v -o "logs/sigma_db-${species_count}_species.time.log" \
-    bash scripts/run_sigma_db.sh "${species_count}_species" >"logs/sigma_db-${species_count}_species.log"
-done
-
-# analysing samples of different sizes against a db of 5636 species
-for read_count in "${read_counts[@]}"; do
-  echo "Running 'run_sigma_samples.sh' for ${read_count} reads against 5636 species"
-
-  $time -v -o "logs/sigma_samples-${read_count}_reads-5636_species.time.log" \
-    bash scripts/run_sigma_samples.sh "${read_count}" 5636 >"logs/sigma_samples-${read_count}_reads-5636_species.log"
-done
-
-# analysing a sample of 1M reads against dbs of various sizes
-for species_count in "${species_counts[@]:0:4}"; do
-  echo "Running 'run_sigma_samples.sh' for input_1M reads against ${species_count} species"
-
-  $time -v -o "logs/sigma_samples-input_1M_reads-${species_count}_species.time.log" \
-    bash scripts/run_sigma_samples.sh input_1M "${species_count}" >"logs/sigma_samples-input_1M-${species_count}_species.log"
-done
-
-# ----------------------------------------------------------------------------------------------------------------------
-# -                                     run all the tests for malt/hops                                                -
-# ----------------------------------------------------------------------------------------------------------------------
-
-# building databases of different sizes
-for species_count in "${species_counts[@]}"; do
-  echo "Running 'run_malt_db.sh' for ${species_count} species"
-
-  $time -v -o "logs/malt_db-${species_count}_species.time.log" \
-    bash scripts/run_malt_db.sh "${species_count}_species" >"logs/malt_db-${species_count}_species.log"
-done
-
-# analysing samples of different sizes against a db of 5636 species
-for read_count in "${read_counts[@]}"; do
-  echo "Running 'run_malt_samples.sh' for ${read_count} reads against 5636 species"
-
-  $time -v -o "logs/malt_samples-${read_count}_reads-5636_species.time.log" \
-    bash scripts/run_malt_samples.sh "${read_count}_reads" 5636 >"logs/malt_samples-${read_count}_reads-5636_species.log"
-done
-
-# analysing a sample of 1M reads against dbs of various sizes
-for species_count in "${species_counts[@]:0:4}"; do
-  echo "Running 'run_malt_samples.sh' for input_1M reads against ${species_count} species"
-
-  $time -v -o "logs/malt_samples-input_1M_reads-${species_count}_species.time.log" \
-    bash scripts/run_malt_samples.sh input_1M_reads "${species_count}" \
-    >"logs/malt_samples-input_1M_reads-${species_count}_species.log"
 done
--- performance_tests/setup_performance_test.sh
+++ performance_tests/setup_performance_test.sh
@@ -7,9 +7,9 @@
 
 # create a new conda environment to run all the test_sets
 eval "$(conda shell.bash hook)"
-conda env remove --name performance_test
-conda env create -f environment.yaml -n performance_test
-conda activate performance_test
+conda env remove --name small_test
+conda env create -f environment.yaml -n small_test
+conda activate small_test
 
 # bash strict mode (set after conda is activated, as the start-up scripts are not strict safe)
 set -euo pipefail
@@ -20,63 +20,14 @@ time=$(which time)
 # set some config options for haystack
 haystack config \
   --api-key 4a7ceec35df93baf39e6f747e08f69f39909 \
-  --cache ./haystack_genome_cache/
+  --cache ./haystack_genome_cache_small/
 
 # fetch all the genomes in the representative RefSeq database
 $time -v haystack database \
   --mode fetch \
-  --accessions ./haystack_configs/haystack_db_5636_species_refseq_input.txt \
-  --output ./genome_fetch/ \
+  --accessions ./haystack_configs/haystack_db_100_species_refseq_input.txt \
+  --output ./genome_fetch_small/ \
   --force-accessions
-
-# download and extract Bracken (as there is no conda package)
-wget --quiet https://github.com/jenniferlu717/Bracken/archive/v2.5.tar.gz -O Bracken-2.5.tar.gz
-tar xvzf Bracken-2.5.tar.gz
-rm Bracken-2.5.tar.gz
-bash Bracken-2.5/install_bracken.sh
-
-# download and extract Sigma (as there is no conda package)
-wget --quiet --content-disposition http://sourceforge.net/projects/sigma-omicsbio/files/V1.0.3/sigma-v1.0.3.tar.gz/download \
-  -O Sigma.tar.gz
-tar xvzf Sigma.tar.gz
-rm Sigma.tar.gz
-
-# setup a list of the different test_sets to run
-test_sets=(10_species 100_species 500_species 1000_species 5636_species)
-
-# get the max available memory (in GB)
-if ! command -v free &> /dev/null; then
-  # MacOS does not have the free command
-  MAX_MEM=$(sysctl -a | awk '/^hw.memsize:/{printf "%.0f", $2/(1024)^3}')
-else
-  # but linux does
-  MAX_MEM=$(free -m | awk '/^Mem:/{printf "%.0f", $2/1024}')
-fi
-
-# configure MALT to use the max available memory
-sed -i'' "s/-Xmx.*/-Xmx${MAX_MEM}G/" ~/.conda/pkgs/malt-0.41-1/opt/malt-0.41/malt-run.vmoptions
-sed -i'' "s/-Xmx.*/-Xmx${MAX_MEM}G/" ~/.conda/pkgs/malt-0.41-1/opt/malt-0.41/malt-build.vmoptions
-
-mkdir -p db_mutlifasta_inputs
-
-# create mutlifasta files for kraken2 and MALT, and move them into the right folders
-for test_set in "${test_sets[@]}"; do
-  cat "genome_paths/${test_set}_refseq.txt" | xargs zcat >"db_mutlifasta_inputs/db_input_${test_set}.fasta"
-done
-
-# download mapping for MALT
-wget --quiet -P mapping_files https://software-ab.informatik.uni-tuebingen.de/download/megan6/megan-map-Jul2020-2.db.zip
-#wget --quiet -P mapping_files https://software-ab.informatik.uni-tuebingen.de/download/megan6/megan-nucl-Jul2020.db.zip
-unzip mapping_files/megan-map-Jul2020-2.db.zip -d mapping_files/
-
-# make the database folders for Sigma by symlinking the genomes into the relevant folders
-for test_set in "${test_sets[@]}"; do
-  mkdir -p "sigma_db_${test_set}"
-  cat "genome_paths/${test_set}_refseq.txt" | xargs dirname | awk '{print "../"$0}' | xargs ln -s -t "sigma_db_${test_set}"
-done
-
-# decompress the genome and input files for Sigma
-gzip -k -d sigma_db_5636_species/*/*.fasta.gz
 
 # download a large HMP sample
 mkdir -p inputs
@@ -92,7 +43,7 @@ cat inputs/SRS078671/SRS078671.denovo_duplicates_marked.trimmed.1.fastq \
   inputs/SRS078671_aggregate.fastq
 
 # subsample the file into smaller samples
-read_counts=(input_10K input_100K input_1M input_10M input_100M)
+read_counts=(input_10K input_100K)
 size=1000
 
 for read_count in "${read_counts[@]}"; do

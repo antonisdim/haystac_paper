2dce13d8aca92d2f976464de3a3808a481da94cc
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2020-12-15 12:28:24 +0000

    max mem db for haystack sample

--- performance_tests/scripts/run_haystack_samples.sh
+++ performance_tests/scripts/run_haystack_samples.sh
@@ -13,6 +13,8 @@ sample_input=${args[0]}
 db_input=${args[1]}
 use_conda=${args[2]}
 
+MAX_MEM=$(free -m | awk '/^Mem:/{printf "%.0f", $2}')
+
 #deactivating conda
 haystack config \
   --use-conda "${use_conda}"
@@ -31,6 +33,6 @@ haystack sample \
 
 haystack analyse \
   --mode abundances \
-  --database ./haystack_db_"${db_input}"_input_8000_mem_conda_"${use_conda}" \
+  --database ./haystack_db_"${db_input}"_input_"$MAX_MEM"_mem_conda_"${use_conda}" \
   --sample ./samples/"${sample_input}" \
   --output ./haystack_out_db_"${db_input}"_"${sample_input}"_conda_"${use_conda}"
